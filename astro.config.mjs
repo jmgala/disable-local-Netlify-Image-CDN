460b850cabc68b4d9c6d47a3e63f3058060cba55
Juan Gala <jmgala001@gmail.com>
2025-11-17 19:07:01 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+    image: {
+        domains: ["githubusercontent.com"],
+    },
     adapter: netlify({
+        imageCDN: false,
         devFeatures: {
             environmentVariables: true,
             images: false,
